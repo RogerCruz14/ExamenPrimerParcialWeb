24b3ab341cbd9294c8a381edd6d4f1b9ffe22df0
Roger <roger.cruzbsc@gmail.com>
2021-11-25 15:51:46 -0500

    Examen del primer Parcial

--- mongodb.js
+++ mongodb.js
@@ -1,15 +1,7 @@
 const mongoose = require('mongoose');
 const connection_string = process.env.MONGO_URI;
 
-mongoose.connect(connection_string, { 
-
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true 
-
-}).then( () => { console.log('Ya esta conectado');} ).catch( err => {console.log(err);} );
-
+mongoose.connect(connection_string, { useNewUrlParser:true, useUnifiedTopology:true })
 
 
 
